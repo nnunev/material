4922cc9fcec90c5e7df5d5b7737eb98d758fe3d0
nnunev <nickolaynunev@gmail.com>
2022-08-28 12:19:56 +0300

    Environment fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MaterialTable from 'material-table'
+
+const empList = [
+  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', status: 0, role: 1 },
+  { id: 2, name: "Raj", email: 'raj@gmail.com', status: 1, role: 0 },
+  { id: 3, name: "David", email: 'david342@gmail.com', status: 1, role: 3 },
+  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', status: 0, role: 2 },
+]
 
 function App() {
+
+  const [data, setData] = useState(empList)
+  const columns = [
+    { title: "ID", field: "id" },
+    { title: "Name", field: "name" },
+    { title: "Email", field: "email" }, 
+    { title: "Status", field: 'status', },
+    { title: "Role", field: "role", }
+  ] 
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 align="center">React-App</h1>
+      <h4 align='center'>Material Table</h4>
+      <MaterialTable
+        title="Employee Data"
+        data={data}
+        columns={columns}
+      />
     </div>
   );
 }
